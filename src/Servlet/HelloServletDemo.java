56c57780ef64b136dc9559426e75cd629d749476
gangping.liu <gangping.liu@ucarinc.com>
2019-07-10 13:18:22 +0800

    修改HelloServletDemo

--- src/Servlet/HelloServletDemo.java
+++ src/Servlet/HelloServletDemo.java
@@ -2,9 +2,13 @@ package Servlet;
 
 /**
  * description TODO
- * author TODO
+ * author gangping.liu
  * date 2019/7/10 13:08
  * version TODO
+ * @author Administrator
  */
 public class HelloServletDemo {
+    public static void main(String[] args) {
+        System.out.println("This is Hello Servlet Demo");
+    }
 }
